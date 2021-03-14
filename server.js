a1ad5ef1b7f8d803e3d0869b2c7def7e2cdbce31
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-03-13 19:17:37 -0500

    added middleware for error handling

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ app.use(morgan(morganSetting));
 app.use(cors());
 app.use(helmet());
 app.use(validateBearerToken);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
 // ----------------------------------------
 // routes
